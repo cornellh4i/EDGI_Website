13a503949c7fdeb614a7434b2f2220e7838235e2
Hubert He <hmh92@cornell.edu>
2023-11-04 14:22:04 -0400

    added button functionality working on styling

--- guide_sidebar.py
+++ guide_sidebar.py
@@ -1,7 +1,30 @@
-import time
 import streamlit as st
 
-with st.sidebar:
-    st.header("Sidebar Content")
+# Initialize a session state variable that tracks the sidebar state (either 'expanded' or 'collapsed').
+if 'sidebar_state' not in st.session_state:
+    st.session_state.sidebar_state = 'expanded'
 
-st.title("Main Content")
+# Streamlit set_page_config method has a 'initial_sidebar_state' argument that controls sidebar state.
+st.set_page_config(initial_sidebar_state=st.session_state.sidebar_state)
+
+m = st.markdown("""
+<style>
+div.stButton > button:first-child {
+    color: black;
+    border-radius: 50%;
+    
+}
+</style>""", unsafe_allow_html=True)
+
+# Show title and description of the app.
+st.title('Example: Controlling sidebar programmatically')
+st.sidebar.markdown(
+    'This is an example Streamlit app to show how to expand and collapse the sidebar programmatically.')
+
+button = st.button('Open &#xa; Guide')
+
+# Toggle sidebar state between 'expanded' and 'collapsed'.
+if button:
+    st.session_state.sidebar_state = 'collapsed' if st.session_state.sidebar_state == 'expanded' else 'expanded'
+    # Force an app rerun after switching the sidebar state.
+    st.experimental_rerun()
