5ad52beb7d92306b036826acbdd5df9986aaa435
Owen Chen <owenchen502@gmail.com>
2023-11-12 21:00:35 -0500

    changed searchbox.py content

--- searchbox.py
+++ searchbox.py
@@ -1,100 +1,162 @@
-import folium
-import pandas as pd
 import streamlit as st
+import pandas as pd
+import numpy as np
+import folium
+from folium.plugins import FastMarkerCluster
 from streamlit_folium import st_folium
-from folium.plugins import Search
-import geopandas
-import json
+from ECHO_modules.get_data import get_echo_data # This function does the work of retrieving data from the SBU database
+from ECHO_modules.get_data import get_spatial_data # This function will help us get county boundaries
+from ECHO_modules.geographies import spatial_tables, fips, region_field, states # Import for mapping purposes
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
-# Create GeoDataFrame for states
-states_gdf = geopandas.read_file(
-    "https://raw.githubusercontent.com/python-visualization/folium-example-data/main/us_states.json",
-    driver="GeoJSON",
-)
+# Styles for States ("other") and selected regions (e.g. Zip Codes) - "this"
+map_style = {'this': {'fillColor': '#0099ff', 'color': '#182799', "weight": 1},
+'other': {'fillColor': '#FFA500', 'color': '#182799', "weight": 1}}
+
+# Get list of counties in the format COUNTY, STATE
+url = "https://raw.githubusercontent.com/edgi-govdata-archiving/"
+url += "ECHO_modules/packaging/data/state_counties_corrected.csv"
+all_counties = pd.read_csv( url )
+column_1 = 'County'
+column_2 = 'FAC_STATE'
+all_counties['County_Pairs'] = all_counties[column_1] + ', ' + all_counties[column_2]
+county_list = all_counties['County_Pairs']
+county_list = county_list.unique()
+
+user_selection = st.selectbox(
+    'Search for a county',
+    county_list)
+comma_pos = user_selection.find(',')
+selected_state = user_selection[comma_pos + 2:]
+selected_county = user_selection[:comma_pos]
 
 
-def convert_state_fp(code):
+def format_county(selected_county):
     '''
-    Returns the state name represented by the two-digit American National Standards Institute (ANSI) Code
+    Returns the county with the first letter of each word capitalized.
+
+    Example: format_county('SAN FRANCISCO') returns 'San Francisco'
     '''
-    states = {"01" : "Alabama", "02" : "Alaska", "04" : "Arizona", "05" : "Arkansas", "06": "California", "08": "Colorado", "09" : "Connecticut", "10" : "Delaware", "11" : "District of Columbia", "12": "Florida", "13": "Georgia", "15": "Hawaii", "16" : "Idaho", "17" : "Illinois", "18" : "Indiana", "19" : "Iowa", "20" : "Kansas", "21" : "Kentucky", "22" : "Louisiana", "23" : "Maine", "24" : "Maryland", "25" : "Massachusetts", "26" : "Michigan", "27": "Minnesota", "28": "Mississippi", "29" : "Missouri", "30" : "Montana", "31" : "Nebraska", "32" : "Nevada", "33" : "New Hampshire", "34" : "New Jersey", "35" : "New Mexico", "36" : "New York", "37" : "North Carolina", "38" : "North Dakota", "39": "Ohio", "40": "Oklahoma", "41" : "Oregon", "42": "Pennsylvania", "44": "Rhode Island", "45" : "South Carolina", "46": "South Dakota", "47": "Tennessee", "48": "Texas", "49" : "Utah", "50": "Vermont", "51": "Virginia", "53": "Washington", "54": "West Virginia", "55": "Wisconsin", "56":"Wyoming", "72":"Puerto Rico"}
+    res = ''
+    county_words = selected_county.split(' ')
+    for word in county_words:
+        res += word.lower().capitalize() + ' '
+    return res[:-1]
 
-    return states[code]
+county_name = format_county(selected_county)
 
-# Read counties GeoJSON
-f = open("data\counties.geojson",)
-data = json.load(f)
 
-# Add state names to each county name in counties.geojson
-for feature in data["features"]:
-    feature['properties']['NAME'] = feature['properties']['NAME'] + ", " + \
-        convert_state_fp(feature['properties']['STATEFP'])
+st.header('Get Facilities in Selected County')
+# Get county names based on Steve's work.
+counties = pd.read_csv("https://raw.githubusercontent.com/edgi-govdata-archiving/ECHO_modules/main/data/state_counties_corrected.csv") # Get a county name lookup
+counties = counties.groupby(by=["FAC_STATE", "County", "FAC_COUNTY"]).count() # Restructure the data
+counties = counties.iloc[(counties.index.get_level_values('County') == selected_county) & (counties.index.get_level_values('FAC_STATE') == selected_state)].reset_index() # Search for COUNTY, STATE
+counties = list(counties["FAC_COUNTY"].unique()) # List FAC_COUNTY names in ECHO associated with COUNTY
+# Format a sql query
+c = "("
+for county in counties:
+  c += "\'"+county+"\',"
+c = c[:-1] + ")"
+sql = 'select * from "ECHO_EXPORTER" where "FAC_COUNTY" in ' + c + ' and "FAC_STATE" = \'' + selected_state + '\'' # "ECHO_EXPORTER" contains basic info about all regulated industrial facilities, including location
+fac = get_echo_data(sql)
+# display facilities in county
+fac
 
-f.close()
 
-# Create GeoDataFrame for counties
-counties_gdf = geopandas.GeoDataFrame.from_features(data["features"])
-counties_gdf = counties_gdf.set_crs("EPSG:4326")
+st.header('Get County Boundaries')
+# This one gets a little complicated because of how the data are structured and the fact that a county name may not be unique (e.g. there are multiple Cedar Counties in the US
+statefp = fips[selected_state] # Get state's code
+sql = 'select * from "tl_2020_us_county" where "name" = \''+county_name +'\' and "statefp" = \''+str(statefp)+'\'' # Select specific county depending on state code
+county, state = get_spatial_data("County", [selected_state], spatial_tables, fips, county_name)
+# display county boundaries
+county
 
-# Create map
-m = folium.Map(location=[38, -97], zoom_start=4)
 
-# Add counties to map
-county_geo = folium.GeoJson(
-    counties_gdf,
-    name="US Counties",
-    style_function=lambda feature: {
-        "fillColor": "#00000000",
-        "color": "black",
-        "weight": 1,
-        "fillOpacity": 0,
-    },
-    highlight_function=lambda feature: {
-        "fillColor": "#FF0000",
-        "color": "#9A0000",
-        "weight": 2,
-        "fillOpacity": 0.2,
-    },
-    tooltip=folium.GeoJsonTooltip(
-        fields=["NAME"], aliases=[""], localize=True
-    ),
-).add_to(m)
+def marker_text( row, no_text ):
+    '''
+    Create a string with information about the facility or program instance.
+    
+    Returns the text (a str) to attach to the marker
 
-# Add states to map
-state_geo = folium.GeoJson(
-    states_gdf,
-    zoom_on_click=True,
-    name="US States",
-    style_function=lambda feature: {
-        "fillColor": "#00000000",
-        "color": "black",
-        "weight": 2,
-        "fillOpacity": 0,
-    },
-    highlight_function=lambda feature: {
-        "fillColor": "#FF0000",
-        "color": "#9A0000",
-        "weight": 2,
-        "fillOpacity": 0.2,
-    },
-    tooltip=folium.GeoJsonTooltip(
-        fields=["name"], aliases=[""], localize=True
-    ),
-).add_to(m)
+    Parameters
+    ----------
+    row : Series
+        Expected to contain FAC_NAME and DFR_URL fields from ECHO_EXPORTER
+    no_text : Boolean
+        If True, don't put any text with the markers, which reduces chance of errors 
+    '''
 
-# Add county search bar
-county_search = Search(
-    layer=county_geo,
-    geom_type="Polygon",
-    placeholder="Search for a county",
-    collapsed=False,
-    search_label="NAME",
-    weight=3,
-).add_to(m)
+    text = ""
+    if ( no_text ):
+        return text
+    if ( type( row['FAC_NAME'] == str )) :
+        try:
+            text = row["FAC_NAME"] + ' - '
+        except TypeError:
+            print( "A facility was found without a name. ")
+        if 'DFR_URL' in row:
+            text += " - <p><a href='"+row["DFR_URL"]
+            text += "' target='_blank'>Link to ECHO detailed report</a></p>" 
+    return text
 
-# Display the map
-folium.LayerControl().add_to(m)
 
-st_data = st_folium(m, width=725)
+def bivariate_map(regions, points, bounds=None, no_text=False):
+    '''
+    Show the map of region(s) (e.g. zip codes) and points (e.g. facilities within the regions)
+
+    Create the map using a library called Folium (https://github.com/python-visualization/folium)
+
+    Bounds can be preset if necessary
+
+    no_text errors can be managed
+    '''
+    m = folium.Map()  
+
+    # Show the region(s
+    s = folium.GeoJson(
+      regions,
+      style_function = lambda x: map_style['other']
+    ).add_to(m)
+
+    # Show the points
+    ## Create the Marker Cluster array
+    mc = FastMarkerCluster("")
+ 
+    # Add a clickable marker for each facility
+    for index, row in points.iterrows():
+      if ( bounds is not None ):
+        if ( not check_bounds( row, bounds )):
+          continue
+      mc.add_child(folium.CircleMarker(
+        location = [row["FAC_LAT"], row["FAC_LONG"]],
+        popup = marker_text( row, no_text ),
+        radius = 8,
+        color = "black",
+        weight = 1,
+        fill_color = "orange",
+        fill_opacity= .4
+      ))
+    
+    m.add_child(mc)
+
+    # compute boundaries so that the map automatically zooms in
+    bounds = m.get_bounds()
+    m.fit_bounds(bounds, padding=0)
+
+    # display the map!
+    st_data = st_folium(m, width=725)
+
+st.header('Map Boundaries and Facilities')
+bivariate_map(county, fac)
+
+
+st.header('Get Environmental Justice Data')
+# Based on this https://www.census.gov/programs-surveys/geography/guidance/geo-identifiers.html
+baseline = int(str(county["geoid"][0]) + "0000000") # Find the county's geoid and turn it into a 12 digit block group code
+next = baseline + 10000000 # Find the next county's geoid - we don't want any of this so it's the limit of our query
+sql = 'SELECT * from "EJSCREEN_2021_USPR" where "ID" between '+str(baseline)+' and '+str(next)+''
+# The above query should give us all the block groups in the county, nothing more nothing less
+justice_data = get_echo_data(sql)
+# display environmental justice data
+justice_data
