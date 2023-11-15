4dcf2c87d638eca52eac1a19c23f5a3ad0150115
Owen Chen <owenchen502@gmail.com>
2023-11-15 14:41:32 -0500

    removed unnecessary comments in region_data.py

--- region_data.py
+++ region_data.py
@@ -2,7 +2,6 @@ import pdb
 import git
 import os
 import streamlit as st
-# from ECHO_modules.geographies import states
 import pandas as pd
 
 
@@ -13,14 +12,6 @@ try:
 except git.GitCommandError:
     g = git.cmd.Git("EEW_County_ReportCards")
     g.pull()
-# except:
-    # region = git.Git(
-    #     "https://github.com/edgi-govdata-archiving/EEW-ReportCard-Data.git")
-    # region.pull(
-    #     "https://github.com/edgi-govdata-archiving/EEW-ReportCard-Data.git", "main", rebase=True)
-
-# git.Repo.clone_from(
-#     "https://github.com/edgi-govdata-archiving/ECHO_modules", "ECHO_modules")
 
 try:
     repo_link = "https://github.com/edgi-govdata-archiving/ECHO_modules"
@@ -57,34 +48,6 @@ for this_state in states:
     df.insert(0, 'State', this_state)
     df_list.append(df)
 all_df = pd.concat(df_list)
-# print( "State violations - {}".format(this_state) )
-# print(df)
 st.write(all_df)
 all_df.to_json('state_violations.json', orient='records')
 all_df.to_csv('state_violations.csv')
-
-
-# def get_county(row, state):
-#     programs = ['CAA', 'CWA', 'RCRA']
-#     region = Region(type='County', state=state, value=row[1].county,
-#                     programs=programs)
-#     df = region.get_per_1000(type='violations', region='County', year=2022)
-#     df.insert(0, 'State', state)
-#     df.insert(1, 'County', row[1].county)
-#     return df
-
-
-# state = 'NY'
-# region = Region(type='None')
-# counties = region.get_counties_by_state(state)
-# print(counties)
-
-# all_df = pd.DataFrame(columns=['State', 'County', 'Program', 'Per1000'])
-# df_list = []
-# for row in counties.iterrows():
-#     df = get_county(row, state)
-#     df_list.append(df)
-# all_df = pd.concat(df_list)
-# # Write the results to a file, two ways
-# all_df.to_json('cd_violations.json', orient='records')
-# all_df.to_csv('cd_violations.csv')
