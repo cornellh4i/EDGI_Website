0858abe3dbf978619caa6262767b693fb7b08426
Owen Chen <owenchen502@gmail.com>
2023-12-03 19:42:38 -0500

    consolidated styling, added comments

--- frontend/header.py
+++ frontend/header.py
@@ -3,11 +3,9 @@ import streamlit as st
 st.set_page_config(page_title="Page Title", layout="wide")
 
 # Connect file to style.css
-with open("css/style.css") as f:
+with open("../css/style.css") as f:
     st.markdown(
-        f"""<style>{f.read()}</style> <body>
-
-  </body>""",
+        f"""<style>{f.read()}</style>""",
         unsafe_allow_html=True,
     )
 
