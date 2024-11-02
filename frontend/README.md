24d3e70c4bb000b0ad5974fc627da5ed6e3cf2e2
Eric Nost <ericnost@gmail.com>
2024-11-02 13:33:56 -0400

    reorg

---
+++ frontend/README.md
@@ -0,0 +1,2 @@
+# EEW_County_ReportCards
+This repository contains code and data for generating Environmental Enforcement Watch reports for U.S. counties.
--- frontend/region_data.py
+++ frontend/region_data.py
@@ -4,8 +4,8 @@ import streamlit as st
 import pandas as pd
 import altair as alt
 
-from EEW_County_ReportCards.Region import Region
-from EEW_County_ReportCards.AllPrograms_util import get_region_rowid
+from Region import Region
+from AllPrograms_util import get_region_rowid
 from ECHO_modules.geographies import states
 
 def get_sidebar_grades(state_name, county_name, types):
